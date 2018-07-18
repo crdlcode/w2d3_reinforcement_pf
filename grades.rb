da45c44af693a2476aebcdd2a1b046ad83329ec3
curtischatch <curtis.chat.hk@gmail.com>
2018-07-18 12:17:19 -0400

    fixed if statements

--- grades.rb
+++ grades.rb
@@ -1,20 +1,25 @@
-def grade(num)
-  num = num.to_f
-
-if (num >=90) && (num <=100)
-  return "A"
-elsif (num <=89) && (num >=80)
-  return "B"
-elsif (num <=79) && (num >=70)
-  return "C"
-elsif (num <=69) && (num >=50)
-  return "D"
-elsif (num >=49) && (num >=0)
-  return "F"
-else
-  return nil
+def letter_grade
+  print "Enter a % grade:"
+  percent = gets.to_i
+  new_grade(percent)
 end
 
+def new_grade(num)
+  num = num.to_i
 
-print "enter your percentage and get your grade"
-puts "you letter grade is "
+  if (num >=90) && (num <=100)
+    return "A"
+  elsif (num <=89) && (num >=80)
+    return "B"
+  elsif (num <=79) && (num >=70)
+    return "C"
+  elsif (num <=69) && (num >=50)
+    return "D"
+  elsif (num <=49) && (num >=0)
+    return "F"
+  else
+    return nil
+  end
+end
+
+puts "you letter grade is #{letter_grade}"
